e0b0873e186e233be0a87ac336aede0cf0c91052
Arinzechukwu Edmund <92803301+arinze96@users.noreply.github.com>
2022-12-01 01:19:00 +0100

    Fresh Commit

--- resources/views/includes/home_css.blade.php
+++ resources/views/includes/home_css.blade.php
@@ -24,6 +24,28 @@
 <!-- jquery js -->
 <script src="{{ asset('assets/newfile2/js/jquery-3.3.1.min.js') }}"></script>
 
+<script type="text/javascript">
+    (function() {
+        var options = {
+            whatsapp: "+19194211323",
+            call_to_action: "Message us",
+            position: "left",
+        };
+        var proto = document.location.protocol,
+            host = "getbutton.io",
+            url = proto + "//static." + host;
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function() {
+            WhWidgetSendButton.init(host, proto, options);
+        };
+        var x = document.getElementsByTagName('script')[0];
+        x.parentNode.insertBefore(s, x);
+    })();
+</script>
+
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
